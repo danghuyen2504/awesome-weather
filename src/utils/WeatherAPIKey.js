0096b7821e2408f518952dc22d666e929454646b
Hai Dang <danghuyen2504@gmai.com>
2018-04-16 09:34:54 +0700

    utils

---
+++ src/utils/WeatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = '504fefa06dcd91bbb941c2f37dd26d3e';
---
+++ src/utils/WeatherConditions.js
@@ -0,0 +1,47 @@
+export const weatherConditions = {
+    Rain: {
+        title: 'Mưa',
+        icon: 'weather-rainy',
+        background: require('../images/rain.png'),
+    },
+    Clear: {
+        title: 'Nắng',
+        icon: 'weather-sunny',
+        background: require('../images/clear.jpg'),
+    },
+    ThunderStorm: {
+        title: 'Mưa dông',
+        icon: 'weather-lightning',
+        background: require('../images/thunderstorm.jpg'),
+    },
+    Clouds: {
+        title: 'Nhiều mây',
+        icon: 'weather-cloudy',
+        background: require('../images/clouds.jpg'),
+    },
+    Snow: {
+        title: 'Tuyết rơi',
+        icon: 'weather-snowy',
+        background: require('../images/snow.jpg'),
+    },
+    Drizzle: {
+        title: 'Mưa nhỏ',
+        icon: 'weather-hail',
+        background: require('../images/drizzle.jpg'),
+    },
+    Haze: {
+        title: 'Sương mù',
+        icon: 'weather-hail',
+        background: require('../images/haze.jpg'),
+    },
+    Mist: {
+        title: 'Sương mù',
+        icon: 'weather-fog',
+        background: require('../images/mist.jpg'),
+    },
+    undefined: {
+        title: 'Undefined',
+        icon: 'cancel',
+        background: require('../images/undefined.png'),
+    }
+};
